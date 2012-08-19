e6a29ca38b1375e26928534cdfb0f24e3ff439a3
Rob <rob@mintsoft.net>
2012-08-19 15:35:38 +0100

    First commit - first rough version

---
+++ README.md
@@ -0,0 +1,5 @@
+Small Bash Script to glue together cdparanoia, lame and cddb for ripping CDs
+to high quality VBR MP3s
+
+## Debian: Probably best using http://deb-multimedia.org/
+## Prerequisites: apt-get install cddb cdparanoia cd-discid lame mp3gain
---
+++ ripCD.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+TEMPDIR="/media/md1_storage/tmp/music"
+
+##prerequisites: apt-get install cddb cdparanoia cd-discid
+pushd $(pwd);
+cd $TEMPDIR;
+
+trackNames=();
+artistName="";
+albumName="";
+
+#Just take the first match
+QUERYSTRING=$(cddbcmd -h freedb.freedb.org cddb query $(cd-discid) |
+				awk 'NR == 1 {print $1,$2}');
+
+while read line; do
+	INDEX=${line%%=*};
+	VALUE=${line#*=};
+	case "$INDEX" in
+		#extract Disc Title
+		DTITLE)
+			artistName="${VALUE%% /*}";	#everything before " /"
+			albumName="${VALUE#*/ }";	#everything after "/ "
+		;;
+		#Extract Track names
+		TTITLE*)
+			trackNum=${INDEX#TTITLE};	    #remove TTITLE from the front to leave the number
+			trackNum=$(($trackNum + 1));	#0 based to 1 based
+			trackNames[${trackNum}]="$VALUE";
+		;;
+		#Ignore everything else
+		*)	;;
+	esac
+done < <(cddbcmd -h freedb.freedb.org cddb read ${QUERYSTRING});
+
+cdparanoia -B
+
+tracknum=1;
+for f in track*.cdda.wav; do
+
+	lame -m j --replaygain-accurate -q 0 --vbr-new -V 0 -b 128 \
+			--ta "${artistName}" --tl "${albumName}" --tn "${tracknum}" \
+			--tt "${trackNames[${tracknum}]}" --add-id3v2 \
+			"${f}" "${f/%wav/mp3}";
+
+	##not entirely sure that --replaygain-accurate does what I want/need so I'll reapply
+	mp3gain -r -c -d 3.0 "${f/%wav/mp3}";
+
+	#Rename target MP3
+	displaytracknum=$(printf "%02i" "${tracknum}");
+	mv -n "${f/%wav/mp3}" "${artistName} - ${albumName} - ${displaytracknum} - ${trackNames[$tracknum]}.mp3"
+
+	tracknum=$(($tracknum+1));
+done
+
+echo "CD Ripped, and should be here: ${TEMPDIR}/*.mp3"
+popd
